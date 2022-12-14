b9fa067c66248416c85e9ad4fc5adddfb53b2df0
Alec Loudenback <alecloudenback@gmail.com>
2022-12-13 20:23:57 -0600

    add examples to docstring

--- src/LifeContingencies.jl
+++ src/LifeContingencies.jl
@@ -700,7 +700,33 @@ end
     decrement(lc::LifeContingency,to_time)
     decrement(lc::LifeContingency,from_time,to_time)
 
-Return the probablity of death for the given LifeContingency. 
+Return the probability of death for the given LifeContingency, with decrements beginning at time zero. 
+    
+# Examples
+
+```julia-repl
+julia> q = [.1,.2,.3,.4];
+
+julia> l = SingleLife(mortality=q);
+
+julia> survival(l,1)
+0.9
+
+julia> decrement(l,1)
+0.09999999999999998
+
+julia> survival(l,1,2)
+0.8
+
+julia> decrement(l,1,2)
+0.19999999999999996
+
+julia> survival(l,1,3)
+0.5599999999999999
+
+julia> decrement(l,1,3)
+0.44000000000000006
+```
 """
 mt.decrement(lc::LifeContingency, from_time, to_time) = 1 - survival(lc.life, from_time, to_time)
 
@@ -709,7 +735,34 @@ mt.decrement(lc::LifeContingency, from_time, to_time) = 1 - survival(lc.life, fr
     survival(lc::LifeContingency,from_time,to_time)
     survival(lc::LifeContingency,to_time)
 
-Return the probability of survival for the given LifeContingency. 
+Return the probability of survival for the given LifeContingency, with decrements beginning at time zero. 
+    
+# Examples
+
+```julia-repl
+julia> q = [.1,.2,.3,.4];
+
+julia> l = SingleLife(mortality=q);
+
+julia> survival(l,1)
+0.9
+
+julia> decrement(l,1)
+0.09999999999999998
+
+julia> survival(l,1,2)
+0.8
+
+julia> decrement(l,1,2)
+0.19999999999999996
+
+julia> survival(l,1,3)
+0.5599999999999999
+
+julia> decrement(l,1,3)
+0.44000000000000006
+    
+    ```
 """
 mt.survival(lc::LifeContingency, to_time) = survival(lc.life, 0, to_time)
 mt.survival(lc::LifeContingency, from_time, to_time) = survival(lc.life, from_time, to_time)
